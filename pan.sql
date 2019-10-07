3dca8ede3a67a84b10601cf1c90330bc1b6256d2
quhailong <hailong@loyalone.cn>
2019-10-07 09:32:56 +0800

    更新数据库脚本

--- pan.sql
+++ pan.sql
@@ -1,108 +1,84 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : mycat
-Source Server Version : 50629
-Source Host           : 118.89.238.129:8066
-Source Database       : pan
-
-Target Server Type    : MYSQL
-Target Server Version : 50629
-File Encoding         : 65001
-
-Date: 2018-07-03 15:31:08
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for capacity
--- ----------------------------
-DROP TABLE IF EXISTS `capacity`;
 CREATE TABLE `capacity` (
-  `uid` varchar(255) NOT NULL,
-  `total` bigint(20) NOT NULL,
-  `used` bigint(20) NOT NULL,
-  PRIMARY KEY (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '用户ID',
+  `total_capacity` bigint(20) DEFAULT NULL COMMENT '总容量',
+  `used_capacity` bigint(20) DEFAULT NULL COMMENT '使用容量',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='容量表';
 
--- ----------------------------
--- Table structure for file
--- ----------------------------
-DROP TABLE IF EXISTS `file`;
+
 CREATE TABLE `file` (
-  `fid` varchar(255) NOT NULL,
-  `originalName` varchar(255) NOT NULL,
-  `size` int(11) NOT NULL,
-  `fileType` int(11) NOT NULL,
-  `fileLocation` varchar(255) NOT NULL,
-  `createtime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `md5` varchar(255) NOT NULL,
-  PRIMARY KEY (`fid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `file_id` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '文件ID',
+  `original_name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '源名称',
+  `file_size` int(11) DEFAULT NULL COMMENT '文件大小',
+  `file_type` int(11) DEFAULT NULL COMMENT '文件类型',
+  `file_location` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '文件地址',
+  `file_md5` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '文件MD5',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_file_id` (`file_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='文件信息表';
 
--- ----------------------------
--- Table structure for share
--- ----------------------------
-DROP TABLE IF EXISTS `share`;
+
 CREATE TABLE `share` (
-  `sid` varchar(255) NOT NULL,
-  `shareId` varchar(255) NOT NULL,
-  `theme` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `uid` varchar(255) NOT NULL,
-  `isLock` int(11) NOT NULL,
-  `sPassword` varchar(255) DEFAULT NULL,
-  `visitTime` int(11) NOT NULL,
-  `saveTime` int(11) NOT NULL,
-  `downloadTime` int(11) NOT NULL,
-  `createtime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `multi` int(11) NOT NULL,
-  `expiration` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`sid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `share_id` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '分享ID',
+  `theme` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '分享主题',
+  `user_id` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '用户ID',
+  `lock_whether` int(11) DEFAULT NULL COMMENT '是否加密',
+  `share_password` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '分享密码',
+  `visit_count` int(11) DEFAULT NULL COMMENT '浏览次数',
+  `save_count` int(11) DEFAULT NULL COMMENT '保存次数',
+  `download_count` int(11) DEFAULT NULL COMMENT '下载次数',
+  `multi_whether` int(11) DEFAULT NULL COMMENT '是否多文件',
+  `expiration` datetime DEFAULT NULL COMMENT '过期时间',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_share_id` (`share_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='分享表';
 
--- ----------------------------
--- Table structure for sharemap
--- ----------------------------
-DROP TABLE IF EXISTS `sharemap`;
-CREATE TABLE `sharemap` (
-  `mid` varchar(255) NOT NULL,
-  `shareId` varchar(255) NOT NULL,
-  `vid` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`mid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for userinfo
--- ----------------------------
-DROP TABLE IF EXISTS `userinfo`;
-CREATE TABLE `userinfo` (
-  `uid` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL,
-  `salt` varchar(255) NOT NULL,
-  `picLocation` varchar(255) NOT NULL,
-  `createtime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `updatetime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `share_map` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `share_id` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '分享ID',
+  `virtual_address_id` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '虚拟地址ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='分享文件对应表';
 
--- ----------------------------
--- Table structure for virtualaddress
--- ----------------------------
-DROP TABLE IF EXISTS `virtualaddress`;
-CREATE TABLE `virtualaddress` (
-  `vid` varchar(255) NOT NULL,
-  `fid` varchar(255) DEFAULT NULL,
-  `uid` varchar(255) NOT NULL,
-  `fileName` varchar(255) NOT NULL,
-  `addrType` int(11) NOT NULL,
-  `createtime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `updatetime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `md5` varchar(255) DEFAULT NULL,
-  `parentPath` varchar(255) NOT NULL,
-  `fileSize` int(11) DEFAULT NULL,
-  `isDir` int(11) NOT NULL,
-  PRIMARY KEY (`vid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `user_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '用户ID',
+  `user_name` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '用户名',
+  `password` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '密码',
+  `phone` varchar(11) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '手机号',
+  `salt` varchar(16) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '盐',
+  `pic_location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '头像地址',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_user_id` (`id`),
+  UNIQUE KEY `index_user_name` (`user_name`),
+  UNIQUE KEY `index_phone` (`phone`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='用户信息表';
+
+
+CREATE TABLE `virtual_address` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `uuid` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '业务ID',
+  `file_id` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '文件ID',
+  `user_id` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '用户ID',
+  `file_name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '文件名称',
+  `addr_type` int(11) DEFAULT NULL COMMENT '虚拟地址类型',
+  `file_md5` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '文件md5',
+  `parent_path` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '父级路径',
+  `file_size` int(11) DEFAULT NULL COMMENT '文件大小',
+  `dir_whether` int(11) DEFAULT NULL COMMENT '是否是文件',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_uuid` (`uuid`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='虚拟地址表';
