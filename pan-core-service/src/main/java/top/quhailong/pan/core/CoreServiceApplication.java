cc762667ca5f99302fd92cd9365e5e7316cd469f
quhailong <hailong@loyalone.cn>
2020-08-11 22:10:55 +0800

    去掉重复注解，增加@EnableDiscoveryClient注解

--- pan-core-service/src/main/java/top/quhailong/pan/core/CoreServiceApplication.java
+++ pan-core-service/src/main/java/top/quhailong/pan/core/CoreServiceApplication.java
@@ -3,13 +3,14 @@ package top.quhailong.pan.core;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-@Configuration
 @SpringBootApplication
 @MapperScan(value = "top.quhailong.pan.core.dao")
+@EnableDiscoveryClient
 public class CoreServiceApplication {
 	public static void main(String[] args) {
         SpringApplication.run(CoreServiceApplication.class, args);
