7c0835a5fa355bafc4d397ae353f772194fd7b63
quhailong <hailong@loyalone.cn>
2020-08-22 23:43:57 +0800

    修改其他模块的配置文件，都改为了nacos作为注册中心和配置中心

--- pan-edge-service/src/main/java/top/quhailong/pan/edge/EdgeServiceApplication.java
+++ pan-edge-service/src/main/java/top/quhailong/pan/edge/EdgeServiceApplication.java
@@ -3,13 +3,14 @@ package top.quhailong.pan.edge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 @SpringBootApplication
-@EnableEurekaClient
-public class EdgeServiceApplication extends SpringBootServletInitializer{
-	public static void main(String[] args) {
+@EnableDiscoveryClient
+public class EdgeServiceApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
         SpringApplication.run(EdgeServiceApplication.class, args);
     }
 
--- pan-file-zuul/src/main/java/top/quhailong/pan/file/zuul/FileZuulApplication.java
+++ pan-file-zuul/src/main/java/top/quhailong/pan/file/zuul/FileZuulApplication.java
@@ -2,7 +2,7 @@ package top.quhailong.pan.file.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,20 +11,21 @@ import org.springframework.web.filter.CorsFilter;
 
 /**
  * 系统核心网关
- * @author 屈海龙
  *
+ * @author 屈海龙
  */
 @SpringBootApplication
-@EnableEurekaClient
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class FileZuulApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FileZuulApplication.class, args);
-	}
-	@Bean
+    public static void main(String[] args) {
+        SpringApplication.run(FileZuulApplication.class, args);
+    }
+
+    @Bean
     public CorsFilter corsFilter() {
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // 允许cookies跨域
         //config.addAllowedOrigin("http://localhost:8082");// #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
